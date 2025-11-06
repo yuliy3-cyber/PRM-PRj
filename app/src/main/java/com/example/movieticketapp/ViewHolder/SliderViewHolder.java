7209e06a1dbdbfbce0c2334ab67455818ed8e04e
yuliy3-cyber <datgosuyt@gmail.com>
2025-11-06 22:41:52 +0700

    holder

---
+++ app/src/main/java/com/example/movieticketapp/ViewHolder/SliderViewHolder.java
@@ -0,0 +1,23 @@
+package com.example.movieticketapp.ViewHolder;
+
+//import com.example.movieticketapp.R;
+//import com.makeramen.roundedimageview.RoundedImageView;
+//
+//public class SliderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+//private RoundedImageView imageView;
+//private TextView textView;
+//
+//public SliderViewHolder(@NonNull View itemView) {
+//        super(itemView);
+//        imageView = (RoundedImageView) itemView.findViewById(R.id.postSlider);
+//        textView = (TextView) itemView.findViewById(R.id.namePost);
+//        }
+//        void SetImage(PostItem postItem){
+//        imageView.setImageResource(postItem.getImage());
+//        }
+//
+//    @Override
+//    public void onClick(View view) {
+//
+//    }
+//}
