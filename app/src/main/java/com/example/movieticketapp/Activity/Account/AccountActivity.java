a8d4f51afe531fde988a980a01930a83f9695ef3
yuliy3-cyber <datgosuyt@gmail.com>
2025-11-11 12:05:29 +0700

    user service

--- app/src/main/java/com/example/movieticketapp/Activity/Account/AccountActivity.java
+++ app/src/main/java/com/example/movieticketapp/Activity/Account/AccountActivity.java
@@ -1,5 +1,9 @@
 package com.example.movieticketapp.Activity.Account;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
+
 import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -8,10 +12,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.LinearLayoutCompat;
 
 import com.example.movieticketapp.Activity.Wallet.MyWalletActivity;
 import com.example.movieticketapp.Firebase.FirebaseRequest;
