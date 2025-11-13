7d6c9d26b977ce33e55cedd10d9e1f5157c8a34b
Dung <dunglthe180884@fpt.edu.vn>
2025-11-13 12:40:01 +0700

    add NotificationDetailActivity

--- app/src/main/java/com/example/movieticketapp/Notification/NotificationDetailActivity.java
+++ app/src/main/java/com/example/movieticketapp/Notification/NotificationDetailActivity.java
@@ -1,15 +1,14 @@
-package com.example.movieticketapp.Activity.Notification;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.movieticketapp.Notification;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.movieticketapp.Activity.HomeActivity;
 import com.example.movieticketapp.Activity.Report.ReportActivity;
