c680ef3869a05bb771aad5c72f1e273b0db6b2c2
Dung <dunglthe180884@fpt.edu.vn>
2025-11-13 12:39:04 +0700

    add FireBaseMessagingService

--- app/src/main/java/com/example/movieticketapp/Notification/FirebaseMessagingService.java
+++ app/src/main/java/com/example/movieticketapp/Notification/FirebaseMessagingService.java
@@ -1,27 +1,16 @@
-package com.example.movieticketapp.Activity.Notification;
+package com.example.movieticketapp.Notification;
 
-import android.annotation.SuppressLint;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.IBinder;
 import android.os.Vibrator;
-import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
 
 import com.example.movieticketapp.R;
 import com.google.firebase.messaging.RemoteMessage;
