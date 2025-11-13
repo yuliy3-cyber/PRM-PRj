6085bbe46ff80cd7f9f2c5d3ceede829de92e8b3
manhgg22 <manhgg22@gmail.com>
2025-11-13 20:27:41 +0700

    Commit 6/10: Add Fragments, ViewHolders, Interfaces and Firebase - Copy fragments, view holders, interfaces and Firebase utilities

--- app/src/main/java/com/example/movieticketapp/ViewHolder/SliderViewHolder.java
+++ app/src/main/java/com/example/movieticketapp/ViewHolder/SliderViewHolder.java
@@ -1,5 +1,12 @@
 package com.example.movieticketapp.ViewHolder;
 
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.movieticketapp.Model.FilmModel;
 //import com.example.movieticketapp.R;
 //import com.makeramen.roundedimageview.RoundedImageView;
 //
